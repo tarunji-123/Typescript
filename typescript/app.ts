bacef98073ad464be9799204998c5f1b7ff0bc60
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-07-30 00:08:09 +0530

    about typescript

---
+++ typescript/app.ts
@@ -0,0 +1,58 @@
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button')!;
+
+// const numResults : number[] = [];
+const textResults : string[] = [];
+
+const numResults : Array<number> = [];
+
+type NumOrString = number | string;
+type Result = {val : number ; timeStamp : Date};
+
+//interface is alternative to Result, both are same but write the code is little bit differ.
+interface ResultObj{
+    val : number;
+    timeStamp : Date;
+}
+
+function add(num1 : NumOrString, num2 : NumOrString){
+    if(typeof num1 ==='number' && typeof num2==='number'){
+        return num1 + num2;
+    }else if(typeof num1 ==='string' && typeof num2 === 'string'){
+        return num1 + ' '+ num2;
+    }
+    return +num1 + +num2;
+}
+
+function printResult (resultObj : Result){
+    console.log(resultObj.val);
+}
+
+buttonElement.addEventListener('click',()=>{
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+
+    const result = add(+num1, +num2);
+    numResults.push(result as number);
+
+    const stringResult = add(num1 , num2);
+    textResults.push(stringResult as string);
+
+    // console.log(result);
+    // console.log(stringResult);
+
+    printResult({val : result as number, timeStamp : new Date()});
+    console.log(numResults, textResults);
+})
+
+const myPromise = new Promise <string>((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve ('It worked!');
+    }, 1000);
+})
+
+myPromise.then((result)=>{
+    // console.log(result.split('w'));
+    console.log(result);
+})
